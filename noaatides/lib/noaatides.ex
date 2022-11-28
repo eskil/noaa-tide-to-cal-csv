ad7c5c7379dd16b0ac52986ab6bc2933f2cfce79
Eskil Heyn Olsen <eskil@eskil.org>
2022-11-27 17:22:39 -0800

    Initial commit

---
+++ noaatides/lib/noaatides.ex
@@ -0,0 +1,18 @@
+defmodule Noaatides do
+  @moduledoc """
+  Documentation for `Noaatides`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Noaatides.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ noaatides/test/noaatides_test.exs
@@ -0,0 +1,8 @@
+defmodule NoaatidesTest do
+  use ExUnit.Case
+  doctest Noaatides
+
+  test "greets the world" do
+    assert Noaatides.hello() == :world
+  end
+end
